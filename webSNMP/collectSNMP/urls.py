6552645e02f3a0fdf0f566e01aa5c8ef39269e5a
Gabriel Soares <gedex3d@gmail.com>
2023-04-03 16:49:17 -0300

    View dos Devices feita

--- webSNMP/collectSNMP/urls.py
+++ webSNMP/collectSNMP/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:device_id>/", views.selectOid, name="selectOid"),
+    path("<int:device_id>/<int:oid_id>/", views.viewGraph, name="viewGraph")
 ]
--- webSNMP/collectSNMP/views.py
+++ webSNMP/collectSNMP/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-
+from django.template import loader
 # Create your views here.
 
 from django.http import HttpResponse
 
+from .models import Device
+
+
 def index(request):
-    return HttpResponse("Hello worl!!")
+    device_list = Device.objects.all()
+    template = loader.get_template("collectSNMP/index.html")
+    context = {
+        'device_list': device_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def selectOid(request, device_id):
+    device = Device.objects.get(id=device_id)
+    return HttpResponse("Selecionado device %s" % device.device_name)
+
+
+def viewGraph(request, device_id, oid_id):
+    return HttpResponse("Voce esta vendo o grafico %s do device %s" % oid_id % device_id)
